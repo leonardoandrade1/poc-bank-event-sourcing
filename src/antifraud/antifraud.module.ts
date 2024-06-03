481e4d79c60452e2d5fbadc6dd170c962d987b05
Leonardo de Andrade <leonardoandrade0@outlook.com>
2024-06-03 11:03:17 -0300

    chore: add CQRS module

--- src/antifraud/antifraud.module.ts
+++ src/antifraud/antifraud.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AntifraudAccountCreationAnalysisController } from './infra/http/account-creation-analysis/account-creation-analysis.controller';
+import { CqrsModule } from '@nestjs/cqrs';
 
 @Module({
+  imports: [CqrsModule.forRoot()],
   controllers: [AntifraudAccountCreationAnalysisController],
   providers: [],
 })
